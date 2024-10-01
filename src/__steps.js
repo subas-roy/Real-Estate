30b4917cf0bd577bca6a5b2605f9665509a6cf2c
subas-roy <subasroy46@gmail.com>
2024-10-01 12:57:37 +0600

    Project and firebase setup

---
+++ src/__steps.js
@@ -0,0 +1,22 @@
+/***
+ * --------------------------
+ *    PROJECT SETUP
+ * -------------------------
+ * 1. Create  project (react router - Tutorial)
+ * 2. Tailwind 
+ * 3. DaisyUI
+ * ----------------------------
+ *    Firebase Setup
+ * ---------------------------
+ * 4. visit: console.firebase.google.com
+ * 5. create project (skip google analytics)
+ * 6. register app (create config)
+ * 7. install firebase: npm install firebase
+ * 8. add config file to your project
+ * 9. Danger: do not publish or make firebase config to public by pushing to github
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: APIKEY,
+  authDomain: AUTHDOMAIN,
+  projectId: PROJECTID,
+  storageBucket: STORAGEBUCKET,
+  messagingSenderId: MESSAGINGSENDERID,
+  appId: APPID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
